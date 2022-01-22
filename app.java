387f8b3f74d7682405794eb273ff73bddc2d71d0
CalciumCrusader <44983783+MrSkeletall@users.noreply.github.com>
2022-01-21 16:14:29 -0800

    added a new method that is importent I swear

--- app.java
+++ app.java
@@ -4,5 +4,10 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public static int addTwo(int n) {
+        return n + 2;
+    }
     
 }
